d96748998b58265254479273eec04eb1663dccac
mar <marilirulita@gmail.com>
2022-08-11 04:08:40 -0700

    add custom methods in post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,15 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
   belongs_to :author, class_name: "User"
+
+  #A method that updates the posts counter for a user.
+  def self.update_post_counter(user)
+    User.find(user.id).increment!(:posts_counter)
+  end
+
+  #A method which returns the 5 most recent comments for a given post.
+  def self.recent_comments(post)
+    Comment.where(post_id: post.id).limit(5).order(created_at: :desc)
+  end
+    
 end
