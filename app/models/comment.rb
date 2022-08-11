634829b5c7024be1a0eba3d6dea50840ae56e63f
mar <marilirulita@gmail.com>
2022-08-11 04:26:10 -0700

    Fix linters

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,8 @@
 class Comment < ApplicationRecord
-  belongs_to :author, class_name: "User"
+  belongs_to :author, class_name: 'User'
   belongs_to :post
 
-  #A method that updates the comments counter for a post.
+  # A method that updates the comments counter for a post.
   def self.update_comment_counter(post)
     Post.find(post.id).increment!(:comments_counter)
   end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,8 +1,8 @@
 class Like < ApplicationRecord
-  belongs_to :author, class_name: "User"
+  belongs_to :author, class_name: 'User'
   belongs_to :post
 
-  #A method that updates the likes counter for a post.
+  # A method that updates the likes counter for a post.
   def self.update_like_counter(post)
     Post.find(post.id).increment!(:likes_counter)
   end
