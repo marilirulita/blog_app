3aeada958cec5404cbab4330c7c97945e15271ef
mar <marilirulita@gmail.com>
2022-08-12 01:15:03 -0700

    Add tests to comments nodel:

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe Like, type: :model do
-  subject { Like.new(author_id: 134, post_id: 90) }
+RSpec.describe Comment, type: :model do
+  subject { Comment.new(author_id: 134, post_id: 90, text: "I like this Post") }
 
   before { subject.save }
 
@@ -12,6 +12,11 @@ RSpec.describe Like, type: :model do
 
   it "author id should be present" do
     subject.author_id = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "text should be present" do
+    subject.text = nil
     expect(subject).to_not be_valid
   end
 
