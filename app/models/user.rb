dacc0b49cb602277034fcbd158e1ee2f0493e037
mar <marilirulita@gmail.com>
2022-08-11 22:59:09 -0700

    add test for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,11 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  validates :name, presence: true
+  validates :image, presence: true
+  validates :bio, presence: true
+  validates :posts_counter, presence: true, comparison: { greater_than_or_equal_to: 0 }
+
   # A method that returns the 3 most recent posts for a given user.
   def self.recent_posts(user)
     Post.where(author_id: user.id).limit(3).order(created_at: :desc)
