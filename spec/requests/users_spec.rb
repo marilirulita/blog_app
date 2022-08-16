202782c775556afda8b1c7677ef844060d9c6ddc
mar <marilirulita@gmail.com>
2022-08-16 10:04:58 -0700

    Add render template test in post and user requests

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,16 +2,26 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
+    before(:example) { get '/users' }
+
     it 'returns http success' do
-      get '/users'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'renders index template' do
+      expect(response).to render_template('index') 
     end
   end
 
   describe 'GET /show' do
+    before(:example) { get '/users/1' }
+
     it 'returns http success' do
-      get '/users/1'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'renders show template' do
+      expect(response).to render_template('show') 
     end
   end
 end
