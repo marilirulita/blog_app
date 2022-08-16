5eea86599aa88e8fbc5ae773b4385c6ec188aa26
mar <marilirulita@gmail.com>
2022-08-16 09:58:01 -0700

    add rspec test for post and users request

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -3,15 +3,21 @@ require 'rails_helper'
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
     it 'returns http success' do
-      get '/posts/index'
+      get '/users/1/posts'
       expect(response).to have_http_status(:success)
     end
   end
 
   describe 'GET /show' do
     it 'returns http success' do
-      get '/posts/show'
+      get '/users/1/posts/show'
       expect(response).to have_http_status(:success)
     end
   end
 end
+
+# If response status was correct.
+# If a correct template was rendered.
+
+#expect(response).to redirect_to(path) 
+# If the response body includes correct placeholder text.
