0cd27c287dfa94112066fc4f3fac2372edafe7c2
mar <marilirulita@gmail.com>
2022-08-17 23:42:51 -0700

    add seed data in db seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+first_user = User.create(name: 'Jhon', image: 'https://icon-library.com/images/person-png-icon/person-png-icon-29.jpg', bio: 'Teacher from Mexico.', posts_counter: 0)
+second_user = User.create(name: 'Jenny', image: 'https://icon-library.com/images/person-png-icon/person-png-icon-29.jpg', bio: 'Teacher from Mexico.', posts_counter: 0)
+third_user = User.create(name: 'Mike', image: 'https://icon-library.com/images/person-png-icon/person-png-icon-29.jpg', bio: 'Teacher from Mexico.', posts_counter: 0)
+
+first_post = Post.create(author_id: first_user.id, title: 'Hello', text: 'This is my first post', comments_counter: 0, likes_counter: 0)
+second_post = Post.create(author_id: first_user.id, title: 'Hello', text: 'This is my second post', comments_counter: 0, likes_counter: 0)
+third_post = Post.create(author_id: first_user.id, title: 'Hello', text: 'This is my third post', comments_counter: 0, likes_counter: 0)
+fourth_post = Post.create(author_id: first_user.id, title: 'Hello', text: 'This is my fourth post', comments_counter: 0, likes_counter: 0)
+
+Comment.create(post_id: first_post.id, author_id: second_user.id, text: 'Hi Tom!' )
+Comment.create(post_id: first_post.id, author_id: second_user.id, text: 'Hi Jenny!' )
+Comment.create(post_id: first_post.id, author_id: second_user.id, text: 'Hi Mike!' )
+Comment.create(post_id: first_post.id, author_id: second_user.id, text: 'Hi Peter!' )
+Comment.create(post_id: first_post.id, author_id: second_user.id, text: 'Hi Luke!' )
+Comment.create(post_id: first_post.id, author_id: second_user.id, text: 'Hi Steve!' )
+
