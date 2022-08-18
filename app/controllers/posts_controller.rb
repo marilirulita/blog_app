336470a78cb128da990e121a78a6b9670e7769c9
mar <marilirulita@gmail.com>
2022-08-17 23:05:22 -0700

    remove logic unecesary from views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
   def index
+    puts params
+   # @author = User.find(comment.author_id).name
     @user = User.find(params[:user_id])
     @posts = Post.where(author_id: @user.id).order(created_at: :desc)
   end
