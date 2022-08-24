7ebb6c8ee32f099dd04dd744e4f2c860ab1e8bd4
mar <marilirulita@gmail.com>
2022-08-24 12:53:21 -0700

    change routs paths in post insdex

--- spec/views/posts/index.html.erb_spec.rb
+++ spec/views/posts/index.html.erb_spec.rb
@@ -12,42 +12,47 @@ RSpec.describe 'posts/index.html.erb', type: :system do
     end
 
     it 'show user profile picture' do
-      visit user_path(@first_user)
+      visit user_posts_path(@first_user)
       expect(page).to have_css("img[src*='https://icon-library.com/images/person-png-icon/person-png-icon-29.jpg']")
     end
 
     it 'show user name' do
-      visit user_path(@first_user)
+      visit user_posts_path(@first_user)
       expect(page).to have_content('Bob')
     end
 
     it 'see number of posts user has written' do
-      visit user_path(@first_user)
+      visit user_posts_path(@first_user)
       expect(page).to have_content('0')
     end
 
     it 'see post title' do
-      visit user_path(@first_user)
+      visit user_posts_path(@first_user)
       expect(page).to have_content('Hello')
     end
 
     it 'see first comment on post' do
-      visit user_path(@first_user)
+      visit user_posts_path(@first_user)
       expect(page).to have_content('This is my first post')
     end
 
     it 'see how many comments on post' do
-      visit user_path(@first_user)
+      visit user_posts_path(@first_user)
       expect(page).to have_content('0')
     end
 
     it 'see how many likes on post' do
-      visit user_path(@first_user)
+      visit user_posts_path(@first_user)
+      expect(page).to have_content('0')
+    end
+
+    it 'see a section for pagination' do
+      visit user_posts_path(@first_user)
       expect(page).to have_content('0')
     end
 
     it 'when click on post, go to post show page' do
-      visit user_path(@first_user)
+      visit user_posts_path(@first_user)
       click_on 'Hello'
       expect(page).to have_content('This is my first post')
     end
